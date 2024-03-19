ef0b2306e1fd8cf3b71862b25a9c44f31da80ea3
Matthew Gerry <mgerry04@gmail.com>
2024-03-19 16:25:00 -0400

    Modified correlation code to save data to a file. Next step is to refactor and modularize so it is a separate script into which we load the data

--- fcs_correlation.m
+++ fcs_correlation.m
@@ -7,28 +7,37 @@
 
 % Matthew Gerry, March 2024
 
+% Current date and time for filenames
+time_string = string(datetime("now",Format="yy-MM-dd_HHmm"));
+
 % Set time values for dynamics
-dt = 40.0;
-tmax = 3200;
+dt = 80.0;
+tmax = 4800;
 time = 0:dt:tmax;
 
 % Set parameter values required for generating a set of chains
+numsites = 961; % Chain length
 p = 0.5; % Proportion of A-type sites
-b = 0.0; % Bias
-numsites = 721; % Chain length
 site_list = -floor(numsites/2):floor(numsites/2);
 epsilon = 0.05; % Tolerance for NN-correlation values
 set_size = 10; % Number of realizations at each c value
 
-c_list = -1.0:0.1:1.0; % Range of correlation values
+cmin = -1.0; cmax = 1.0; dc = 0.1;
+c_list = cmin:dc:cmax; % Range of correlation values
 seed_list = 1:set_size; % List of random seeds to cycle through at each c-value
 
 % Set parameter values required for generating rate matrices
 tau = 1.0;
 ga_av = 1.0; dga = 1.0;
 ga_a = ga_av + 0.5*dga; ga_b = ga_av - 0.5*dga;
+b = 0.0; % Bias
 
+% Store all parameter values in a dictionary
+param_names = ["dt","tmax","numsites","p","epsilon", "set_size","cmin","cmax","dc","tau","ga_av","dga","b"];
+param_vals = [dt, tmax, numsites, p, epsilon, set_size, cmin, cmax, dc, tau, ga_av, dga, b];
+param = dictionary(param_names, param_vals);
 
+%%
 % Pre-allocate a 3-index tensor whose slices correspond to different c-values
 % Each row in a slice is to be a different chain realization
 chains = zeros(length(c_list), set_size, numsites);
@@ -44,7 +53,6 @@ for ii=1:length(c_list)
 
     end % jj
 end % ii
-
 
 % Pre-allocate a 4-index tensor to store time-series data for each chain
 % including probability distributions and their time derivatives
@@ -62,6 +70,13 @@ for ii=1:length(c_list)
     end % jj
 end % ii
 
+% Save parameters, chains, and distributions to file
+save(strcat("../RWdata_",time_string),"param","chains","dists","dpdt")
+
+%%
+
+load("RWdata_24-03-19_1607.mat") % Load the data back in
+% Will need to re-define some of the parameter-dependent varaibles
 
 % Calculate statistics
 site_tensor = repmat(reshape(site_list,[1,1,numsites,1]),[length(c_list),set_size,1,length(time)]);
@@ -78,7 +93,7 @@ D = 0.5*sum(dpdt.*(site_tensor.^2),3) - n_av.*v_av;
 % At a few choices of correlation value
 
 figure;
-c_indices = [2,11,20];
+c_indices = [3,11,19];
 
 for ii=1:length(c_indices)
     c = c_list(c_indices(ii));
@@ -97,10 +112,10 @@ end
 %% Animate probability distribution over time
 figure;
 for kk=1:length(time)
-    bar(site_list, reshape(dists(20,3,:,kk),[1,numsites]))
+    bar(site_list, reshape(dists(10,3,:,kk),[1,numsites]))
     ylim([0,0.1])
     drawnow
-    pause(0.02)
+    pause(0.06)
 end
 
 
