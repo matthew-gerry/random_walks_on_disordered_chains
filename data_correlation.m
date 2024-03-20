44d5da624b9570ace923c2643811fea1ee4bbcea
Matthew Gerry <mgerry04@gmail.com>
2024-03-20 13:30:06 -0400

    separated data generation and statistics calculations into two separate matlab scripts. Data is saved to a file in the parent directory

---
+++ data_correlation.m
@@ -0,0 +1,69 @@
+% data_correlation.m
+
+% Through a range of values of nearest-neighbour correlation between
+% site-types, generate a set of chains. Generate probability distributions
+% as a function of time for each chain. This data can be used to make
+% statistical calculations, determining quantities such as the diffusion
+% coefficient given the nearest neghbour correlations. Multiple
+% realizations are used at each correlation value to capture the
+% differences between realizations.
+
+% Matthew Gerry, March 2024
+
+% Current date and time for filenames
+time_string = string(datetime("now",Format="yy-MM-dd_HHmm"));
+
+% Set time values for dynamics
+dt = 80.0;
+tmax = 4000;
+time = 0:dt:tmax;
+
+% Set parameter values required for generating a set of chains
+numsites = 961; % Chain length
+p = 0.5; % Proportion of A-type sites
+site_list = -floor(numsites/2):floor(numsites/2);
+epsilon = 0.05; % Tolerance for NN-correlation values
+set_size = 10; % Number of realizations at each c value
+
+cmin = -1.0; cmax = 1.0; dc = 0.1;
+c_list = cmin:dc:cmax; % Range of correlation values
+seed_list = 1:set_size; % List of random seeds to cycle through at each c-value
+
+% Set parameter values required for generating rate matrices
+tau = 1.0;
+ga_av = 1.0; dga = 1.0;
+ga_a = ga_av + 0.5*dga; ga_b = ga_av - 0.5*dga;
+b = 0.0; % Bias
+
+% Pre-allocate a 3-index tensor whose slices correspond to different c-values
+% Each row in a slice is to be a different chain realization
+chains = zeros(length(c_list), set_size, numsites);
+
+% Populate the chains tensor with according binary sequences
+for ii=1:length(c_list)
+    c = c_list(ii);
+
+    for jj=1:set_size
+        seed = seed_list(jj); % Slightly redundant, but allows us to play around with the seed list
+
+        chains(ii, jj, :) = corr_chain(p, numsites, c, epsilon, seed);
+
+    end % jj
+end % ii
+
+% Pre-allocate a 4-index tensor to store time-series data for each chain
+dists = zeros(length(c_list), set_size, numsites, length(time));
+
+for ii=1:length(c_list)
+    for jj=1:set_size
+        L = L_chain(chains(ii, jj, :), b, ga_a, ga_b, tau);
+
+        PDF_temp = pdf_L(L, dt, tmax);
+        dists(ii, jj, :, :) = PDF_temp;
+
+    end % jj
+end % ii
+
+% Save parameters, chains, and distributions to file
+save(strcat("../RWdata_",time_string),"dt","tmax","numsites","p","epsilon", "set_size","cmin","cmax","dc","tau","ga_av","dga","b","chains","dists")
+
--- fcs_correlation.m
+++ fcs_correlation.m
@@ -1,82 +1,32 @@
 % fcs_correlation.m
 
-% Through a range of values of nearest-neighbour correlation between
-% site-types, generate a set of chains. Perform full counting statistics on
-% each, to establish the values of the cumulants with error due to the 
-% finite-sample size and finite chain length
+% Carry out full counting statistics for random walks on disordered chains
+% with varying nearest-neighbour correlation values. Import data generated
+% by the function data_correlation.m which calculates probability
+% distributions as a function of time for a range of nearest-neighbour
+% correlation values.
 
 % Matthew Gerry, March 2024
 
-% Current date and time for filenames
-time_string = string(datetime("now",Format="yy-MM-dd_HHmm"));
+load("../RWdata_24-03-20_1319.mat") % Load the data in 
 
-% Set time values for dynamics
-dt = 80.0;
-tmax = 4800;
-time = 0:dt:tmax;
-
-% Set parameter values required for generating a set of chains
-numsites = 961; % Chain length
-p = 0.5; % Proportion of A-type sites
-site_list = -floor(numsites/2):floor(numsites/2);
-epsilon = 0.05; % Tolerance for NN-correlation values
-set_size = 10; % Number of realizations at each c value
-
-cmin = -1.0; cmax = 1.0; dc = 0.1;
+% Re-define some of the parameter-dependent variables
+time = 0:dt:tmax; % Time
+site_list = -floor(numsites/2):floor(numsites/2); % List of site indices
 c_list = cmin:dc:cmax; % Range of correlation values
 seed_list = 1:set_size; % List of random seeds to cycle through at each c-value
+ga_a = ga_av + 0.5*dga; ga_b = ga_av - 0.5*dga; % Gamma values
 
-% Set parameter values required for generating rate matrices
-tau = 1.0;
-ga_av = 1.0; dga = 1.0;
-ga_a = ga_av + 0.5*dga; ga_b = ga_av - 0.5*dga;
-b = 0.0; % Bias
-
-% Store all parameter values in a dictionary
-param_names = ["dt","tmax","numsites","p","epsilon", "set_size","cmin","cmax","dc","tau","ga_av","dga","b"];
-param_vals = [dt, tmax, numsites, p, epsilon, set_size, cmin, cmax, dc, tau, ga_av, dga, b];
-param = dictionary(param_names, param_vals);
-
-%%
-% Pre-allocate a 3-index tensor whose slices correspond to different c-values
-% Each row in a slice is to be a different chain realization
-chains = zeros(length(c_list), set_size, numsites);
-
-% Populate the chains tensor with according binary sequences
-for ii=1:length(c_list)
-    c = c_list(ii);
-
-    for jj=1:set_size
-        seed = seed_list(jj); % Slightly redundant, but allows us to play around with the seed list
-
-        chains(ii, jj, :) = corr_chain(p, numsites, c, epsilon, seed);
-
-    end % jj
-end % ii
-
-% Pre-allocate a 4-index tensor to store time-series data for each chain
-% including probability distributions and their time derivatives
-dists = zeros(length(c_list), set_size, numsites, length(time));
+% Calculate time derivative of the probability distribution at each time step
 dpdt = zeros(length(c_list), set_size, numsites, length(time));
-
 for ii=1:length(c_list)
     for jj=1:set_size
-        L = L_chain(chains(ii, jj, :), b, ga_a, ga_b, tau);
-
-        PDF_temp = pdf_L(L, dt, tmax);
-        dists(ii, jj, :, :) = PDF_temp;
-        dpdt(ii, jj, :, :) = L*PDF_temp;
-
+      
+        L = L_chain(chains(ii, jj, :), b, ga_a, ga_b, tau); % Rate matrix for the chain
+        dpdt(ii, jj, :, :) = L*reshape(dists(ii, jj, :, :),[numsites,length(time)]);
+    
     end % jj
 end % ii
-
-% Save parameters, chains, and distributions to file
-save(strcat("../RWdata_",time_string),"param","chains","dists","dpdt")
-
-%%
-
-load("RWdata_24-03-19_1607.mat") % Load the data back in
-% Will need to re-define some of the parameter-dependent varaibles
 
 % Calculate statistics
 site_tensor = repmat(reshape(site_list,[1,1,numsites,1]),[length(c_list),set_size,1,length(time)]);
@@ -112,10 +62,8 @@ end
 %% Animate probability distribution over time
 figure;
 for kk=1:length(time)
-    bar(site_list, reshape(dists(10,3,:,kk),[1,numsites]))
+    bar(site_list, reshape(dists(11,3,:,kk),[1,numsites]))
     ylim([0,0.1])
     drawnow
     pause(0.06)
 end
-
-
